a546694b155762680e42875cf8c2f419b76a434a
Ashwin-1411 <ashwin141105@gmail.com>
2026-03-20 16:47:29 +0530

    feat(core): Add application constants and configuration

---
+++ lib/utils/constants.dart
@@ -0,0 +1,8 @@
+class AppConstants {
+  static const String appName = 'Continuum';
+  static const String tagline = 'Gig Worker Protection. Always on.';
+  static const String defaultZone = 'Bangalore South - HSR';
+  
+  static const double borderRadius = 16.0;
+  static const double cardPadding = 20.0;
+}
